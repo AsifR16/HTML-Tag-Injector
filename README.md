99a3c8908f466449aa99d6afc49a4f20047d93c4
AsifR16 <asifr8054@gmail.com>
2024-03-16 21:33:36 +0530

    v1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 A simple tag injector for HTML, made in Javascript for code reusability.
+
+--------------------How to use----------------
+use the <tag-import> to import an external HTML file into its place.
+use the "src" attribute of <iag-import> to specify the path of the file to be imported and "data-id" attribute to specify the id of the tag
+within which <tag-import> is present.
+One example is already provided with the code
--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-class HTMLExport extends HTMLElement{
+let data_id = null;
+
+class HTMLImport extends HTMLElement{
+    static observedAttributes = ['src','data-id'];
+
     constructor(){
         super();
     }
     connectedCallback(){
-        
+        console.log("Added element to document");
+    }
+    disconnectedCallback(){
+        console.log("Removed element from document");
+    }
+    adoptedCallback(){
+        console.log("Element moved to new document");
+    }
+    attributeChangedCallback(name, oldValue, newValue){
+        if(name=="data-id"){
+            data_id = newValue;
+        }
+        if(name=="src"){
+            fetch(newValue).then(response => response.text()).then(data=>{
+                document.getElementById(data_id).removeChild(document.getElementsByTagName("tag-import")[0]);
+                document.getElementById(data_id).innerHTML = data;
+            });
+        }
     }
 }
+window.customElements.define("tag-import",HTMLImport);
